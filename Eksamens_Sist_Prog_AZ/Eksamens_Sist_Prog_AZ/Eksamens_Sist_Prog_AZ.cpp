d83d1c5d74027cc5f48c15db70b6c21b23775ad0
Zorgenberga <annija.zorgenberga@gmail.com>
2022-06-15 16:04:34 +0300

    Tests

---
+++ Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ.cpp
@@ -0,0 +1,12 @@
+﻿// Eksamens_Sist_Prog_AZ.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int punkti = 0;
+	string jautajumi[] = { "Ar kuriem no šiem datu tipiem var izmantot skaitliskās funkcijas?" };
+	string varianti[] = {}
+}
