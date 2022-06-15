d7e6d9e166f7984a6bb73adc03a64fe04b3852c9
Zorgenberga <annija.zorgenberga@gmail.com>
2022-06-16 00:00:51 +0300

    Pabeigti visi jautajumi un nosacijumi lietotajam

--- Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ.cpp
+++ Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ/Eksamens_Sist_Prog_AZ.cpp
@@ -20,15 +20,33 @@ int main()
 	int punkti = 0, i=0, pagp=0;
 	string nepareizie = "";
 	string jautajumi[] = { "1. Ar kuriem no šiem datu tipiem var izmantot skaitliskās funkcijas?\n",
-							"2. Kuras no šīm funkcijām ir iebūvētas?\n" };
-	string varianti[] = { "a) int\nb) char\nc) double\nd) float\n",
-							"a) pow(a,b)\nb) min(a,b)\nc) sqrt(a)\nd) max(a,b)\n"};
-	string atbildes[2] = {"a,c,d", "b,d"};
-	
-	cout << "Tests par biežāk lietotajām funkcijām darbam ar skaitliskajiem manīgajiem programmēšanas valodā C++!\n\n";
-	cout<<"Atbildes formāts burti alfabēta secībā atdalot ar komatu, piemērs: \"a,b,c\"\n";
+						   "2. Kuras no šīm funkcijām ir iebūvētas?\n",
+						   "3. Kuras no šīm funkcijām nav iebūvētas?\n",
+						   "4. Kuras no šīm funkcijām ir trigonometriskas funkcijas?\n",
+						   "5. Kuras no šīm funkcijām ir nepareizi uzrakstītas?\n",
+						   "6. Kuras no šīm funkcijām nav skaitliskās funkcijas?\n",
+						   "7. Kuras no šīm funkcijām noapaļo skaitli?\n",
+						   "8. Kurām no šīm funkcijām vajag dot vairāk nekā vienu skaitli? \n",
+						   "9. Kuras no šīm funkcijām nav pareizi uzrakstītas? \n",
+						   "10. Kuras no šīm funkcijām nav trigonometriskās funkcijas?"};
 
-	while (i < 2) {
+	string varianti[] = {   "a) int\nb) char\nc) double\nd) float\n",
+							"a) pow(a,b)\nb) min(a,b)\nc) sqrt(a)\nd) max(a,b)\n",
+							"a) sin(a)\nb) abs(a)\nc) max(a,b)\nd) pow(a,b)\n",
+							"a) acos(a)\nb) sin(a)\nc) tan(a,b)\nd) log(a)\n",
+							"a) sinuss(a)\nb) squareroot(a)\nc) abs(a)\nd)  pi_m\n",
+							"a) min(a)\nb) toupper()\nc) sqrt(a)\nd) tolower()\n",
+							"a) sqrt(a)\nb) round(a)\nc) tan(a)\nd) ceil()\n",
+							"a) pow(a, b)\nb) sqrt(a)\nc) min(a, b)\nd) round(a)\n",
+							"a) maximum(a)\nb) sqrt(a)\nc) ceiling(a)\nd) round(a)\n",
+							"a) sin(a)\nb) acos(a)\nc) abs(a, b)\nd) log(a)" };
+
+	string atbildes[] = {"a,c,d", "b,d", "a,b,d", "a,b,c", "a,b,d", "b,d", "b,d", "a,c", "a,c", "c,d"};
+	
+	cout << "10 jautājumu tests par biežāk lietotajām funkcijām darbam ar skaitliskajiem manīgajiem programmēšanas valodā C++!\n\n";
+	cout<<"Uz katra jautājumi ir 2 vai 3 pareizi atbilžu varianti\nAtbildes formāts ir burti alfabēta secībā atdalot ar komatu, piemērs: \"a,b,c\".\n\n";
+
+	while (i < 10) {
 		cout << jautajumi[i];
 		cout << varianti[i];
 		pagp = punkti;
@@ -38,13 +56,16 @@ int main()
 		}
 		i++;
 	}
-	cout << nepareizie;
-	cout << "\nPareizi: " << punkti;
+
+	cout << "\nPareizi atbildēto jautājumu skaits: " << punkti;
 	i = 0;
-	cout << "\nNepareizi atbildētie jautājumi:\n";
-	while (i < nepareizie.length()) {
-		cout << jautajumi[(int)nepareizie[i] - 48];
-		i++;
+	if (nepareizie.length() != 0) {
+		cout << "\nNepareizi atbildētie jautājumi:\n";
+		while (i < nepareizie.length()) {
+			cout << jautajumi[(int)nepareizie[i] - 48];
+			i++;
+		}
 	}
+	else cout << "\nVisas atbildes pareizas!";
 
 }
